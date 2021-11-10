69b09842606f422b3006ffc6c11d1881c5095146
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2021-11-10 19:32:22 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package com.sonata.Assignment;
+
+public class Employee {
+	
+	int empId;
+	String empName;
+	double empSal;
+	
+	public Employee() {}
+		
+		public Employee(int Id, String Name, double sal) {
+			empId = Id;
+			empName = Name;
+			empSal = sal;
+		}
+	
+
+}
